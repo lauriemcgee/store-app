7ec3801a93368c8e79054dcab0852fccb0868480
lauriemcgee <livingoncoffeeandflowers@gmail.com>
2017-03-21 18:54:27 -0500

    initial commit app redo

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,42 @@
+class ProductsController < ApplicationController
+  def index
+    @products = Product.all
+    render "index.html.erb"
+  end
+  def new
+    render "new.html.erb"
+  end
+  def create
+    product = Product.create(
+        name: params[:name],
+        description: params[:description],
+        price: params[:price]
+      )
+    redirect_to "index.html.erb"
+  end
+  def show
+    product_id = params[:id]
+    @product = Product.find_by(id: product_id)
+    render "show.html.erb"
+  end
+  def edit
+    product_id = params[:id]
+    @product = Product.find_by(id: product_id)
+    render "edit.html.erb"
+  end
+  def update
+    product_id = params[:id]
+    product = Product.find_by(id: product_id)
+    product.name = params[:name]
+    product.description = params[:description]
+    product.price = params[:price]
+    product.save
+    redirect_to "index.html.erb"
+  end
+  def destroys
+    product_id = params[:id]
+    product = Product.find_by(id: product_id)
+    product.destroy
+    redirect_to "index.html.erb"
+  end
+end
