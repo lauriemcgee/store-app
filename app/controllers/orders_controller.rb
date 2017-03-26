c0a25458639a50f678b95f553b00fcad291c6b39
lauriemcgee <livingoncoffeeandflowers@gmail.com>
2017-03-26 10:36:34 -0500

    add details to order show page

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,21 +1,24 @@
 class OrdersController < ApplicationController
   def create
     product_id = params[:id]
-    @product = Product.find_by(id: product_id)
-    @product_tax = @product.price * .08
-    @order_total = product.price + product_tax
+    product = Product.find_by(id: params[:product_id])
+    @subtotal = product.price * params[:quantity].to_i
+    @product_tax = @subtotal * 0.09
+    @order_total = @subtotal + @product_tax
+
     order = Order.create(
-        user_id: current_user.id
-        product_id: product_id
-        quantity: params[:quantity]
-        subtotal: product.price
-        tax: product_tax
-        total: order_total
+        user_id: current_user.id,
+        product_id: params[:product_id],
+        quantity: params[:quantity],
+        subtotal: @subtotal,
+        tax: @product_tax,
+        total: @order_total
       )
     flash[:success] = "Successfully ordered product(s)!"
     redirect_to "/orders/#{order.id}"
   end
   def show
+    @order = Order.find_by(id: params[:id])
     render "show.html.erb"
   end
 end
