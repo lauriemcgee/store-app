dd806dc320f71a6124e552f69d406d597b0d8571
lauriemcgee <livingoncoffeeandflowers@gmail.com>
2017-03-27 18:30:56 -0500

    add category_products table

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,8 +1,8 @@
 class OrdersController < ApplicationController
   def create
-    product_id = params[:id]
-    product = Product.find_by(id: params[:product_id])
-    @subtotal = product.price * params[:quantity].to_i
+    product = Product.find_by(id: params[:id])
+    quantity = params[:quantity]
+    @subtotal = product.price * quantity.to_i
     @product_tax = @subtotal * 0.09
     @order_total = @subtotal + @product_tax
 
